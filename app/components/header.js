e7f4f00af0240f36219f1fb9ecc52c25d70916f2
Gong-xi <821793919@qq.com>
2017-08-24 18:34:49 +0800

    first commit

---
+++ app/components/header.js
@@ -0,0 +1,16 @@
+/**
+ * Created by admin on 2017/8/20.
+ */
+import React from "react";
+import "./header.less";
+let Header=React.createClass({
+    render(){
+        return (
+            <div className="components-header row">
+            <img src="static/images/logo.png" width="40" alt="" className="-col-auto"/>
+                <h1 className="caption">React Music Player</h1>
+            </div>
+        )
+    }
+});
+export default Header;
